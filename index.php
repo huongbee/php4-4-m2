b0f7bc7afd69e2cda84375422ae2cb8e7dc86a9c
Huong Huong <Huong Huong>
2017-04-22 16:36:41 +0700

    buoi1

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+
+//echo "Hello cả thế giới"."<br>".'2345678'."<br>";
+
+$chuoi = "xin chào ";
+
+
+$bool = true;
+
+$mang = array('một'=>1,'hai'=>2,"ba"=>3,"năm"=>5);
+$json = json_encode($mang);
+
+
+//echo $mang['hai'];
+$so='';
+
+echo $so;
+
+
+define('pi',3.14);
+//echo pi;
+
+$soa = 10;
+$sob = 12;
+echo $soa+=$sob; //$soa=$soa+$sob
+echo $soa;
+//var_dump($json);
+
+
+//print_r($mang);
+
+
+//gfdsasdf
+/*
+
+ẻhwert
+ẻghfg
+
+*/
+
+
+
+// class test{
+// 	function chao_ban(){
+// 		echo "chào bạn";
+// 	}
+// }
+
+// $obj = new test;
+
+// $obj->chao_ban();
+
+
+?>
